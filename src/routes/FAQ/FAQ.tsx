067e2eee3da1c440cf82771b4fa6f3970f842555
evansloan <evansloan082@gmail.com>
2022-02-03 19:28:39 -0500

    add discord links

--- src/routes/FAQ/FAQ.tsx
+++ src/routes/FAQ/FAQ.tsx
@@ -11,8 +11,15 @@ const FAQ = () => (
       <Col md={4}>
         <h3 className='text-yellow'>Why is my log data incorrect/not updating?</h3>
         <ul>
-          <li>Make sure you have the "Upload collection log data" config enabled in the Runelite Plugin.</li>
+          <li>Make sure you have the "Upload collection log data" config enabled in the Runelite plugin.</li>
           <li>Click through your collection log in game to ensure the plugin has updated collection log data</li>
+        </ul>
+      </Col>
+      <Col md={4}>
+        <h3 className='text-yellow'>Have a question or problem?</h3>
+        <ul>
+          <li>Join the <a href='https://discord.gg/cFVa9BRSEN'>Log hunters Discord server</a></li>
+          <li>Submit an issue on <a href='https://github.com/evansloan/collectionlog.net/issues'>GitHub</a></li>
         </ul>
       </Col>
     </Row>
