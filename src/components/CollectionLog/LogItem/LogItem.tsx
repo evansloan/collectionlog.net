9c8c4395bca4bbddbc6945f5b404a072850887ba
evansloan <evansloan082@gmail.com>
2022-02-07 01:11:27 -0500

    open wiki link in new tab

--- src/components/CollectionLog/LogItem/LogItem.tsx
+++ src/components/CollectionLog/LogItem/LogItem.tsx
@@ -10,7 +10,7 @@ const LogItem = (props: LogItemProps) => (
   <div className='item'>
     <span className='item-quantity'>{props.item.quantity > 0 ? props.item.quantity : '\u00a0'}</span>
     <span className='item-tooltip'>
-      <a href={`https://oldschool.runescape.wiki/w/${props.item.name.replace(/ /g, '_')}`}>{props.item.name}</a>
+      <a href={`https://oldschool.runescape.wiki/w/${props.item.name.replace(/ /g, '_')}`} target='_blank'>{props.item.name}</a>
     </span>
     <div className='item-img-container'>
       <img
