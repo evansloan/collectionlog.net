5f465f5f5d52e413a5dc89bfa0f083619fff1f25
evansloan <evansloan082@gmail.com>
2022-02-22 00:27:15 -0500

    update faq

--- src/routes/FAQ/FAQ.tsx
+++ src/routes/FAQ/FAQ.tsx
@@ -8,14 +8,15 @@ const FAQ = () => (
       <h1 className='text-center'>Frequently Asked Questions</h1>
     </Row>
     <Row>
-      <Col md={4}>
+      <Col md={6}>
         <h3 className='text-yellow'>Why is my log data incorrect/not updating?</h3>
         <ul>
           <li>Make sure you have the "Upload collection log data" config enabled in the Runelite plugin.</li>
           <li>Click through your collection log in game to ensure the plugin has updated collection log data</li>
+          <li>If all else fails, delete your character's collection log save file in the collection log folder under the Runelite folder and click through your collection log again. (Make sure you are logged out of the game when deleting the file)</li>
         </ul>
       </Col>
-      <Col md={4}>
+      <Col md={6}>
         <h3 className='text-yellow'>Have a question or problem?</h3>
         <ul>
           <li>Join the <a href='https://discord.gg/cFVa9BRSEN'>Log hunters Discord server</a></li>
