ce49bd811b88ae7993003b98b7b97edba6e55ffa
evansloan <evan@evans.loan>
2022-03-15 19:14:04 -0400

    move router into its own component

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './index.scss';
 
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import { CollectionLog, FAQ, Hiscores } from './routes/index';
+import Router from './router';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<CollectionLog />} />
-        <Route path='/:username' element={<CollectionLog />} />
-        <Route path='/:username/:entry' element={<CollectionLog />} />
-        <Route path='/hiscores/:type/:page' element={<Hiscores />} />
-        <Route path='/faq' element={<FAQ />} />
-      </Routes>
-    </BrowserRouter>
+    <Router />
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
