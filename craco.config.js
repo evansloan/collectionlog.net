ccda7d152e112bc18052298d7fe68c56c1e98943
evansloan <evan@evans.loan>
2022-05-28 18:12:36 -0400

    fix typo in craco config

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,7 @@ module.exports = {
   webpack: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
-      '@models': path.resolve(__direname, 'src/models'),
+      '@models': path.resolve(__dirname, 'src/models'),
       '@routes': path.resolve(__dirname, 'src/routes'),
       '@store': path.resolve(__dirname, 'src/store'),
       '@utils': path.resolve(__dirname, 'src/utils'),
