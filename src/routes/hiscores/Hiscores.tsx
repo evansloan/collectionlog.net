237bdf5a834e0a8f17e6e572854ca20e17428a7c
evansloan <evan@evans.loan>
2022-06-07 18:00:11 -0400

    remove dependencies from hiscores useEffect

--- src/routes/hiscores/Hiscores.tsx
+++ src/routes/hiscores/Hiscores.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import DocumentMeta from 'react-document-meta';
+import { useParams } from 'react-router';
 
 import { HiscoresList, HiscoresNav } from '@components/hiscores';
 import { Container } from '@components/layout';
 import { capitalize } from '@utils/format';
 import { useAppDispatch, useAppSelector } from '@store/hooks';
-import { useParams } from 'react-router';
 import { fetchHiscores } from '@store/hiscores/actions';
 import { HiscoresType } from '@store/hiscores/slice';
 
@@ -16,14 +16,14 @@ const Hiscores = () => {
   
   useEffect(() => {
     const hiscoresType = params.type as HiscoresType;
-    let page = parseInt(params.page as string);
+    let page = Number(params.page as string);
   
     if (!page) {
       page = 1;
     } 
 
     dispatch(fetchHiscores(hiscoresType, page, 'ALL'));
-  }, [dispatch, params.type, params.page]);
+  }, []);
 
   const meta = {
     title: `${capitalize(state.type ?? '')} Hiscores | Page ${state.page ?? 1}`,
