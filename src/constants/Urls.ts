2d695706670a7734702b11fedfa74a0df8f6c6ea
kspanier2013 <10370516+kspanier2013@users.noreply.github.com>
2023-03-01 01:18:05 -0500

    Refactor URLs, add Button links

---
+++ src/constants/Urls.ts
@@ -0,0 +1,5 @@
+export const discordUrl = 'https://discord.gg/loghunters';
+
+export const gitHubUrl = 'https://github.com/evansloan/collectionlog.net';
+
+export const pluginUrl = 'https://runelite.net/plugin-hub/show/collection-log';
