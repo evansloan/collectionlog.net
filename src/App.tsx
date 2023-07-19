f5712ee1f5eaa5ecd63f47dcf3a1a1c139768414
Evan Sloan <evan@evans.loan>
2023-07-19 05:57:10 -0400

    add query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { Hiscores, Home, Log } from './pages';
 import { Footer, Header } from './components/layout';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      retryOnMount: false,
+    },
+  },
+});
+
 const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/log/:username' element={<Log />} />
-      <Route path='/log/:username/:entry' element={<Log />} />
-      <Route path='/hiscores' element={<Hiscores />} />
-      <Route path='/hiscores/:page' element={<Hiscores />} />
-    </Routes>
-    <Footer />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/log/:username' element={<Log />} />
+        <Route path='/log/:username/:pageName' element={<Log />} />
+        <Route path='/hiscores' element={<Hiscores />} />
+        <Route path='/hiscores/:page' element={<Hiscores />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </QueryClientProvider>
 );
 
 export default App;
