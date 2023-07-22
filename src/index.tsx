656c4ece6b0a32238f598135760a207f100d3376
Evan Sloan <evan@evans.loan>
2023-07-21 20:01:11 -0400

    use react-query to fetch twitch streams

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 
 import App from './App';
-import { store } from './app/store';
 import AnalyticsService from './services/analytics';
 
 import './index.scss';
@@ -15,7 +13,5 @@ const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <App />
 );
