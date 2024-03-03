f567222b317b4652d6720afd703a487787a29355
Evan Sloan <evan@evans.loan>
2024-03-02 20:28:59 -0500

    update error handling

--- app/error.tsx
+++ app/error.tsx
@@ -1,9 +1,22 @@
 'use client';
 
-const Error = ({ error }: { error: Error & { digest?: string } }) => (
-  <main className='flex h-full flex-col items-center justify-center'>
-    <h2 className='text-center'>{error.message}</h2>
-  </main>
-);
+import { useEffect } from 'react';
+
+type ErrorProps = { error: Error & { digest?: string } };
+
+const Error = ({ error }: ErrorProps) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <main className='flex h-full flex-col items-center justify-center'>
+      <h2 className='text-center'>
+        An error occurred when trying to load the page. Please try again in a
+        moment.
+      </h2>
+    </main>
+  );
+};
 
 export default Error;
