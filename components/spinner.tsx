9bd478699d53820eae55295ee560411b35f8785b
Evan Sloan <evan@evans.loan>
2024-03-15 05:11:48 -0400

    update var name in spinner component

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -3,11 +3,11 @@ interface SpinnerProps {
 }
 
 const Spinner = ({ wrap }: SpinnerProps) => {
-  const spinner = <div className='bit-spinner icon-shadow text-shadow'></div>;
+  const Comp = <div className='bit-spinner icon-shadow text-shadow'></div>;
   return wrap ? (
-    <div className='flex h-[200px] items-center justify-center'>{spinner}</div>
+    <div className='flex h-[200px] items-center justify-center'>{Comp}</div>
   ) : (
-    spinner
+    Comp
   );
 };
 export default Spinner;
